dba21e1ec0ab8cb72341c7d3c2c0861b3d656d19
kak64 <112125903+kak64@users.noreply.github.com>
2026-01-09 09:13:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-﻿#!/bin/bash
-sudo apt update && sudo apt install -y nmap tor proxychains4 metasploit-framework python3-pip stem curl
-pip3 install python-nmap shodan pymetasploit3 colorama requests flask python-telegram-bot --break-system-packages
+#!/bin/bash
+echo "[*] Setting up Apex Framework..."
+sudo apt update
+sudo apt install -y nmap tor proxychains4 metasploit-framework python3-pip curl
+pip3 install python-nmap shodan pymetasploit3 colorama requests flask --break-system-packages
+
+# Setup ProxyChains
 sudo sed -i 's/socks4 \t127.0.0.1 9050/socks5 \t127.0.0.1 9050/' /etc/proxychains4.conf
 sudo service tor start
+
+echo "[+] Done. Remember to start msfrpcd before running core.py"
